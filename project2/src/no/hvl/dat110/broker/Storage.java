7588539d916431a2029fc82de283c4e5006ffef1
571525 <571525@stud.hvl.no>
2019-02-27 15:47:20 +0100

    2.1 DONE

--- project2/src/no/hvl/dat110/broker/Storage.java
+++ project2/src/no/hvl/dat110/broker/Storage.java
@@ -1,89 +1,84 @@
 package no.hvl.dat110.broker;
 
+import no.hvl.dat110.messagetransport.Connection;
+
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
-import no.hvl.dat110.common.Logger;
-import no.hvl.dat110.messagetransport.Connection;
-
 public class Storage {
 
-	protected ConcurrentHashMap<String, Set<String>> subscriptions;
-	protected ConcurrentHashMap<String, ClientSession> clients;
+    protected ConcurrentHashMap<String, Set<String>> subscriptions;
+    protected ConcurrentHashMap<String, ClientSession> clients;
 
-	public Storage() {
-		subscriptions = new ConcurrentHashMap<String, Set<String>>();
-		clients = new ConcurrentHashMap<String, ClientSession>();
-	}
+    public Storage() {
+        subscriptions = new ConcurrentHashMap<String, Set<String>>();
+        clients = new ConcurrentHashMap<String, ClientSession>();
+    }
 
-	public Collection<ClientSession> getSessions() {
-		return clients.values();
-	}
+    public Collection<ClientSession> getSessions() {
+        return clients.values();
+    }
 
-	public Set<String> getTopics() {
+    public Set<String> getTopics() {
 
-		return subscriptions.keySet();
+        return subscriptions.keySet();
 
-	}
+    }
 
-	public ClientSession getSession(String user) {
+    public ClientSession getSession(String user) {
 
-		ClientSession session = clients.get(user);
+        ClientSession session = clients.get(user);
 
-		return session;
-	}
+        return session;
+    }
 
-	public Set<String> getSubscribers(String topic) {
+    public Set<String> getSubscribers(String topic) {
 
-		return (subscriptions.get(topic));
+        return (subscriptions.get(topic));
 
-	}
+    }
 
-	public void addClientSession(String user, Connection connection) {
+    public void addClientSession(String user, Connection connection) {
 
-		// TODO: add corresponding client session to the storage
-		
-		throw new RuntimeException("not yet implemented");
-		
-	}
+        // TODO: add corresponding client session to the storage
+        ClientSession session = new ClientSession(user, connection);
+        clients.put(user, session);
+    }
 
-	public void removeClientSession(String user) {
+    public void removeClientSession(String user) {
 
-		// TODO: remove client session for user from the storage
+        // TODO: remove client session for user from the storage
+        clients.remove(user);
+    }
 
-		throw new RuntimeException("not yet implemented");
-		
-	}
+    public void createTopic(String topic) {
 
-	public void createTopic(String topic) {
+        // TODO: create topic in the storage
+        subscriptions.put(topic, new HashSet<>());
+    }
 
-		// TODO: create topic in the storage
+    public void deleteTopic(String topic) {
 
-		throw new RuntimeException("not yet implemented");
-	
-	}
+        // TODO: delete topic from the storage
+        subscriptions.remove(topic);
+    }
 
-	public void deleteTopic(String topic) {
+    public void addSubscriber(String user, String topic) {
 
-		// TODO: delete topic from the storage
+        // TODO: add the user as subscriber to the topic
+        if (subscriptions.contains(topic)) {
+            subscriptions.get(topic).add(user);
+        }
+    }
 
-		throw new RuntimeException("not yet implemented");
-		
-	}
+    public void removeSubscriber(String user, String topic) {
 
-	public void addSubscriber(String user, String topic) {
-
-		// TODO: add the user as subscriber to the topic
-		
-		throw new RuntimeException("not yet implemented");
-		
-	}
-
-	public void removeSubscriber(String user, String topic) {
-
-		// TODO: remove the user as subscriber to the topic
-
-		throw new RuntimeException("not yet implemented");
-	}
+        // TODO: remove the user as subscriber to the topic
+        if (subscriptions.contains(topic)) {
+            if (subscriptions.get(topic).contains(user))
+                subscriptions.get(topic).remove(user);
+        }
+    }
 }
